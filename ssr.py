38f5ef2b6d43a2a88a27368587366c9fbd80b405
user.name <wistar.ma@gmail.com>
2019-06-12 13:50:49 +0800

    'f'

--- ssr.py
+++ ssr.py
@@ -1,4 +1,26 @@
 import requests
+import os
+import smtplib
+from email.mime.text import MIMEText
+
 r=requests.get(url='https://m.ssrgo.ml/free_ssr')
 
-print(r.text)
+
+smtp_server='smtp.163.com'
+port=0
+sender='wistar@163.com'
+password='xx325622'
+receiver='wistar.ma@gmail.com'
+
+subject='test'
+body=r.text
+msg=MIMEText(body,'plain','utf-8')
+msg['From']=sender
+msg['To']=receiver
+msg['Subject']=subject
+
+print(body)
+with smtplib.SMTP() as smtp:
+    smtp.connect(smtp_server)
+    smtp.login(sender,password)
+    smtp.sendmail(sender,receiver,msg.as_string())
